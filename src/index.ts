955ce985596a07b122dfb7324acea2f22397db8b
naari3 <naari.named@gmail.com>
2019-06-15 23:36:50 +0900

    Filling all

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
-import * as PIXI from "pixi.js"
+/* global document */
 
-let type = "WebGL"
-if (!PIXI.utils.isWebGLSupported()) {
-  type = "canvas"
-}
+import * as PIXI from "pixi.js";
+import blockImage from "./images/n2.png";
 
-PIXI.utils.sayHello(type);
+const width = 10;
+const height = 20;
+
+const displayWidth = width * 16;
+const displayHeight = height * 16;
+
+const app = new PIXI.Application({
+  width: displayWidth,
+  height: displayHeight,
+  backgroundColor: 0x000000
+});
+document.body.appendChild(app.view);
+
+const container = new PIXI.Container();
+app.stage.addChild(container);
+
+const texture = PIXI.Texture.from(blockImage);
+const tilingSprite = new PIXI.TilingSprite(texture, width * 16, height * 16);
+
+container.addChild(tilingSprite);
