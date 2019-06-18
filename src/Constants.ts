d07b613214f175d9283e2fd6e66fbe26ec68f058
naari3 <naari.named@gmail.com>
2019-06-19 03:07:45 +0900

    Changed settings

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,9 +1,9 @@
 export default {
   blockWidth: 10,
   blockHeight: 20,
-  holdMinoScale: 0.5,
+  holdMinoScale: 0.8,
   nextnextMinoScale: 0.8,
-  lockDelayThreashold: 60,
+  lockDelayThreashold: 30,
   controller: {
     up: "w",
     left: "a",
