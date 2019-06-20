4a78c98e284e5ff2bd760c9adfb2760d3759c543
naari3 <naari.named@gmail.com>
2019-06-20 17:36:58 +0900

    Added more settings

--- src/components/Setting.vue
+++ src/components/Setting.vue
@@ -1,8 +1,28 @@
 <template>
   <div>
-    <label for="settingsGravity">gravity:</label>
-    <input v-model.number="settingsGravity" type="number" />
-    <label>{{ Math.round((settingsGravity / 65536) * 100) / 100 }} G</label>
+    <p>
+      <label>gravity:</label>
+      <input v-model.number="settingsGravity" type="number" />
+      <label>{{ Math.round((settingsGravity / 65536) * 100) / 100 }} G</label>
+    </p>
+
+    <p>
+      <label>lock delay time:</label>
+      <input v-model.number="settingsLockDelayTime" type="number" />
+      <label>frame(s)</label>
+    </p>
+
+    <p>
+      <label>ARE time:</label>
+      <input v-model.number="settingsAreTime" type="number" />
+      <label>frame(s)</label>
+    </p>
+
+    <p>
+      <label>line clear time:</label>
+      <input v-model.number="settingsLineClearTime" type="number" />
+      <label>frame(s)</label>
+    </p>
 
     <button @click="doGameStart" v-if="!started">game start</button>
   </div>
@@ -22,7 +42,7 @@ export default class Setting extends Vue {
   started: boolean = false;
 
   @Emit()
-  public input(key: string, value: number) {
+  public input(key: string, value: any) {
     this.settings[key] = value;
   }
 
@@ -32,6 +52,30 @@ export default class Setting extends Vue {
 
   private set settingsGravity(value: number) {
     this.input("gravity", value);
+  }
+
+  private get settingsLockDelayTime(): number {
+    return this.settings.lockDelayTime;
+  }
+
+  private set settingsLockDelayTime(value: number) {
+    this.input("lockDelayTime", value);
+  }
+
+  private get settingsAreTime(): number {
+    return this.settings.lockDelayTime;
+  }
+
+  private set settingsAreTime(value: number) {
+    this.input("areTime", value);
+  }
+
+  private get settingsLineClearTime(): number {
+    return this.settings.lineClearTime;
+  }
+
+  private set settingsLineClearTime(value: number) {
+    this.input("lineClearTime", value);
   }
 
   doGameStart() {
